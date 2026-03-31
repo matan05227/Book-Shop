f21e29ec8b1144a9950734de0cb04030fd3450c1
matan05227 <matan05227@gmail.com>
2026-03-31 12:51:22 +0300

    Update onInit

---
+++ js/book.controler.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function onInit() {
+    renderBooks()
+}
+
+function renderBooks() {
+    
+}
---
+++ js/book.service.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Books = [
+    {
+        id: 'ma455',
+        title: 'The adventures of Lori Ipsi',
+        price: 120,
+    },
+   
+    {
+        id: 'ta899',
+        title: 'World Atlas',
+        price: 300,
+    },
+   
+    {
+        id: 'nc777',
+        title: 'Zobra the Greek',
+        price: 87,
+    }
+
+]
+
+function getBooks() {
+    return Books
+}
