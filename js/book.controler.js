50a72c587abf0947de30131a0a1063970213e0e4
matan05227 <matan05227@gmail.com>
2026-03-31 13:12:03 +0300

    Remove the hard coded

--- js/book.controler.js
+++ js/book.controler.js
@@ -5,5 +5,22 @@ function onInit() {
 }
 
 function renderBooks() {
-    
+     const elBookList = document.querySelector('tbody')
+    const Books = getBooks()
+
+    const strHtmls = Books.map(Book => {
+        return `
+            <tr>
+                <td>${Book.title}</td>
+                <td>${Book.price}</td>
+                <td>
+                    <button>Read</button>
+                    <button>Update</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        `
+    })
+
+    elBookList.innerHTML = strHtmls.join('')
 }
